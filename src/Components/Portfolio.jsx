04c7768eb89691063b721f4f93aa8451014643e0
MUKESH PAL <133365427+mukprabhakar@users.noreply.github.com>
2024-05-13 14:08:14 +0000

    all things changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/saamy1.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "ATM Interface 🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://github.com/mukprabhakar/ATM_INTERFACE",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Online Seat Booking",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "https://github.com/mukprabhakar/online_registration",
   },
   {
-    title: "My Resume Site",
+    title: "Amazon Clone",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/mukprabhakar/amazon-clone",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Matrix Calculator",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://github.com/mukprabhakar/Matrix-Calculator_by_c_langauge",
   },
 ];
 
